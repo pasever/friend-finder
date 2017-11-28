da6e3335c4dfeaa618ef942f88a321b82b5aefb5
Pasever <pas1034ever@gmail.com>
2017-11-27 22:38:59 -0500

    a.0.2

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const PORT = process.env.PORT || 9000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+  console.log("Friends listening on PORT: " + PORT);
+});
